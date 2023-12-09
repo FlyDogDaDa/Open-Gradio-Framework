c73321f381ae831b1e64f2e81fd60b19e842fb0d
飛天柴犬 <71758353+FlyDogDaDa@users.noreply.github.com>
2023-12-09 18:10:34 +0800

    Create 範例

---
+++ WebUI/gradio_pages/Holle.py
@@ -0,0 +1,4 @@
+import gradio as gr
+
+with gr.Blocks(title="Hello") as Holle:
+    gr.Markdown("這是一個測試用的分頁，而且是第二份")
---
+++ WebUI/gradio_pages/ImageClassification.py
@@ -0,0 +1,4 @@
+import gradio as gr
+
+with gr.Blocks(title="Image Classification") as ImageClassification:
+    gr.Markdown("這是一個測試用的分頁，而且是第一份")
